894ba32ac5ba6bfed2e5cd3a9d226eae4a1cac02
reinaka <juliabednaia@gmail.com>
2024-12-17 17:35:21 +0300

    more config settings, application build success

--- services/host/src/components/App.tsx
+++ services/host/src/components/App.tsx
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+import { Link, Outlet} from 'react-router-dom';
+
 export const App = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount((prev) => ++prev);
+    }
+
+    const decrement = () => {
+        if(count > 0) setCount((prev) => --prev);
+    }
+
     return (
-        <div>App</div>
+        <main>
+           <h1>SHOP</h1>
+           <div>
+            <Link to="/page_1">Page 1</Link>
+            <Link to="/page_2">Page 2</Link>
+           </div>
+           <div>
+            <div>{count}</div>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+           </div>
+           <Outlet />
+        </main>
     )
 }
